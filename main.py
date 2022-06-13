3f5cab90d807fdda730ea9cb0724d7e226ace2ff
Benjamin Winstone <>
2022-06-13 23:25:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,157 @@
+
+print("kio ora ")
+n = input("what your name? ")
+print("welcome to the Te Reo maori QUIZ " + n)
+
+
+
+score=0
+print ("question1 no or yes tangata rino is Iron man in maori")
+question = input()
+if question == "yes"or question == "y":
+ print("well done ")
+score=score+1
+if question == "no"or question == "n": 
+ print ("wrong")
+
+
+
+
+print ("question2 yes or no  Spider man is tangata pungawerewere in maori")
+question = input()
+if question == "yes"or question == "y":
+ print("well done ")
+
+ score=score+1
+if question == "no"or question == "n":
+ print ("wrong")
+
+
+print ("question3 yes or no tangata pekapeka is maori for bat man")
+question = input()
+if question == "yes"or question == "y":
+ print("well done ")
+
+score=score+1
+if question == "no"or question == "n":
+ print ("wrong")
+
+
+print ("question4 yes or no tangata nui is maori for super man")
+question = input()
+if question == "yes"or question == "y":
+ print("worng")
+
+ score=score+1
+if question == "no"or question == "n":
+ print ("correct")
+
+print ("question5 yes or no fire is maori for tekau")
+question = input()
+if question == "no"or question == "n":
+ print("well done ")
+
+ score=score+1
+if question == "yes"or question == "y":
+ print ("wrong")
+
+
+print ("question6 yes or no pineapple is maori for Pineamine")
+question = input()
+if question == "no"or question == "n":
+ print("well done ")
+
+ score=score+1
+if question == "yes"or question == "y":
+ print ("wrong")
+
+
+print ("question7 yes or no nine  maori for iwa ")
+question = input()
+if question == "yes"or question == "y":
+ print("well done ")
+
+ score=score+1
+if question == "no"or question == "n":
+ print ("wrong")
+
+       
+print ("question8 yes or no happy in maori is harikoa")
+question = input()
+if question == "yes"or question == "y":
+ print("well done ")
+
+ score=score+1
+if question == "no"or question == "n":
+ print ("wrong")
+
+
+print ("question9 yes or no in fart maori is pāmamao ")
+question = input()
+if question == "yes"or question == "y":
+ print("well done ")
+
+ score=score+1
+if question == "no"or question == "n":
+ print ("wrong")
+       
+print("current score is :"+ str(score))
+
+print ("question10 yes or no fake in maori is tahi ")
+question = input()
+if question == "yes"or question == "y":
+ print("wrong ")
+
+ score=score+1
+if question == "no"or question == "n":
+ print ("correct")
+
+
+print ("question11 yes or no dog in maori is kuri ")
+question = input()
+if question == "yes"or question == "y":
+ print("correct ")
+
+ score=score+1
+if question == "no"or question == "n":
+ print ("wrong")
+
+print ("question12 yes or no bag in  maori is whawhai ")
+question = input()
+if question == "yes"or question == "y":
+ print("wrong")
+
+ score=score+1
+if question == "no"or question == "n":
+ print ("correct")
+
+print ("question13 yes or no fish in maori is ika ")
+question = input()
+if question == "yes"or question == "y":
+ print("correct")
+
+ score=score+1
+if question == "no"or question == "n":
+ print ("wrong")
+
+
+print ("question14 yes or no dog in maori is mapi")
+question = input()
+if question == "yes"or question == "y":
+ print("wrong")
+ score=score+1
+if question == "no"or question == "n":
+ print ("correct")
+
+
+print ("question15 yes or no man  in maori is tangata")
+question = input()
+if question == "yes"or question == "y":
+ print("correct")
+ score=score+1
+if question == "no"or question == "n":
+ print ("wrong")
+print (score)
+
+
+
